993193ec3bbd35b0560327bb4a97366ffe7186d6
Abdallah Ali <amaali1991@gmail.com>
2025-12-22 12:08:41 +0200

    add nushell

--- home.nix
+++ home.nix
@@ -5,4 +5,98 @@
   home.stateVersion = "24.05";
 
   # insert home-manager config
+  programs = {
+    nushell = {
+      enable = true;
+      # for editing directly to config.nu
+      extraConfig = ''
+        let carapace_completer = {|spans|
+        carapace $spans.0 nushell ...$spans | from json
+        }
+        $env.config = {
+          hooks: {
+            pre_prompt: [{ ||
+              if (which direnv | is-empty) {
+                return
+              }
+
+              direnv export json | from json | default {} | load-env
+              if 'ENV_CONVERSIONS' in $env and 'PATH' in $env.ENV_CONVERSIONS {
+                $env.PATH = do $env.ENV_CONVERSIONS.PATH.from_string $env.PATH
+              }
+            }]
+          }
+
+         show_banner: false,
+         completions: {
+         case_sensitive: false # case-sensitive completions
+         quick: true    # set to false to prevent auto-selecting completions
+         partial: true    # set to false to prevent partial filling of the prompt
+         algorithm: "fuzzy"    # prefix or fuzzy
+                    external: {
+         # set to false to prevent nushell looking into $env.PATH to find more suggestions
+             enable: true
+         # set to lower can improve completion performance at the cost of omitting some options
+             max_results: 100
+             completer: $carapace_completer # check 'carapace_completer'
+           }
+         }
+        }
+        $env.PATH = (
+            $env.PATH
+            | split row (char esep)
+            | prepend [
+                $"($env.HOME)/.cargo/bin"          # user cargo bin
+                $"($env.HOME)/.local/bin"  # custom release builds
+                $"($env.HOME)/.config/emacs/bin"
+              ]
+            | flatten
+            | uniq
+        )
+        def dioxus_bundle_github [commit: string]  {
+            cargo clean
+            let current_dir = pwd;
+            rm -rf ($current_dir | path join target/dx/blogy/release/web) ;
+            rm -rf ($current_dir | path join docs);
+            dx bundle --renderer web -r --out-dir docs;
+            let dest = ($current_dir | path join docs)
+            let items = (ls ($current_dir | path join "docs/public/") | get name)
+            if ($items | length) > 0 {
+                mv ...$items $dest
+            }
+            rm -rf ($current_dir | path join "docs/public/" )
+            cp ($current_dir | path join docs/index.html) ($current_dir | path join docs/404.html);
+            let items = (
+                ls ($current_dir | path join "docs/assets/")
+                | where type == file and name =~ "\\.wasm$"
+                | get name
+            )
+            if ($items | length) > 0 {
+                gzip -9 -k ...$items
+            }
+            git add .;
+            git commit -m ($commit);
+            git push;
+        }
+        alias ll = ls -l
+        alias sudo = doas
+      '';
+    };
+
+    carapace = {
+      enable = true;
+      enableNushellIntegration = true;
+    };
+
+    starship = {
+      enable = true;
+      settings = {
+        add_newline = true;
+        character = {
+          success_symbol = "[➜](bold green)";
+          error_symbol = "[➜](bold red)";
+        };
+      };
+    };
+  };
 }
